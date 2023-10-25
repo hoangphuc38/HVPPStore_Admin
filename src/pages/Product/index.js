05c2bed2592eb03010f4f52256582290c3796057
hoangphuc38 <hoangxuanhanductho@gmail.com>
2023-10-26 00:39:41 +0700

    Add sortbar and add-product-btn

--- src/pages/Product/index.js
+++ src/pages/Product/index.js
@@ -2,22 +2,39 @@ import Dropdown from 'react-dropdown';
 import 'react-dropdown/style.css';
 import classNames from 'classnames/bind';
 import styles from './Product.module.scss';
+import Button from '../../components/Button';
+import { AddIcon } from '../../components/Icons';
 
 const cx = classNames.bind(styles);
 
 function Product() {
-    const options = [
-        'one', 'two', 'three'
+    const optionClothes = [
+        'Áo 1', 'Áo 2'
     ];
-    const defaultOption = options[0];
+    const optionSeasons = [
+        '2008', '2009', '2010'
+    ];
+    const defaultOptionClothes = 'Chung';
+    const defaultOptionSeasons = 'Mùa giải';
 
     return (
         <div className={cx('content')}>
-            <Dropdown className={cx('Dropdown-root')}
-                controlClassName={cx('Dropdown-control')}
-                options={options}
-                value={defaultOption}
-                placeholder="Select" />
+            <div className={cx('sortbars-and-button')}>
+                <div className={cx('sortbars')}>
+                    <Dropdown controlClassName={cx('Dropdown-control')}
+                        arrowClassName={cx('Dropdown-arrow')}
+                        options={optionClothes}
+                        value={defaultOptionClothes}
+                        placeholder="Select" />
+
+                    <Dropdown controlClassName={cx('Dropdown-control-season')}
+                        arrowClassName={cx('Dropdown-arrow-season')}
+                        options={optionSeasons}
+                        value={defaultOptionSeasons}
+                        placeholder="Select" />
+                </div>
+                <Button className={cx('add-product-btn')} leftIcon={<AddIcon />}>Thêm sản phẩm</Button>
+            </div>
         </div>
 
     );
