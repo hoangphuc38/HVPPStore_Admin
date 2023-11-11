60e017a117b97e1087d76ce0ebdaa8cd2e4d8504
hoangphuc38 <hoangxuanhanductho@gmail.com>
2023-11-11 22:04:42 +0700

    Fix bugs for UI and add DetailProduct UI

--- src/components/AvatarItem/index.js
+++ src/components/AvatarItem/index.js
@@ -1,5 +1,6 @@
 import classNames from "classnames/bind";
 import styles from './AvatarItem.module.scss';
+import avatar from '../../images/albert-dera-ILip77SbmOE-unsplash.jpg';
 
 const cx = classNames.bind(styles);
 
@@ -7,7 +8,7 @@ function AvatarItem() {
     return (
         <div className={cx('wrapper')}>
             <img className={cx('avatar')}
-                src="https://s3-alpha-sig.figma.com/img/46db/a90c/9e079416f760c0281d83276faaabe0c3?Expires=1698624000&Signature=oHN8ljPKwhmsALhJcBUnTNblDsw0wQ4x-7tggf7l6aj4zSzpgY5UJC-Gknd0~U5nx7mBxVgXqVpZG7WSw8S4NcyHI-MeZYewUA~JxKsSyxpVdcAwlxdg2UKZVce2D~1BMqJO9SszUbrKGn3VJLgrvU7Oo3~c6A4GqK2HFfqoIzqaASG~OLRZUMxPmQOaFlSl6NnS20ntaVVDZd6eOny9r7w8fG11ykWJue52YhBgVZVbJAHlvcBUo0-oXPDChHdO9k7Jf2jUOPuc6CvALypSa7HHbLPLUjA0E3FKvVRWgt4LZyI128-sjjQmJAisdo9TLze0WpQRRuZ3Zq8sgnpghA__&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4"
+                src={avatar}
                 alt="avatar" />
             <div className={cx('info')}>
                 <p className={cx('name')}>
--- src/pages/OrderDetail/index.js
+++ src/pages/OrderDetail/index.js
@@ -1,6 +1,113 @@
+import classNames from 'classnames/bind';
+import styles from './OrderDetail.module.scss';
+import { useState } from 'react';
+import image from '../../images/Ao-real-madrid-san-khach-2023-1.webp';
+
+const cx = classNames.bind(styles);
+
 function OrderDetail() {
+    const [isOpen, setIsOpen] = useState(false);
+
+    const openProductList = () => {
+        setIsOpen(true);
+        console.log("Open ->");
+    }
+
+    const closeProductList = () => {
+        setIsOpen(false);
+        console.log("Close ->");
+    }
+
     return (
-        <div>OrderDetail</div>
+        <div className={cx('container')}>
+            <div className={cx('order-status')}>
+                <div className={cx('title')}>
+                    <div className={cx('icon-status')}></div>
+                    <span className={cx('text-title')}>TRẠNG THÁI ĐƠN HÀNG</span>
+                </div>
+
+                <div className={cx('status-bar-container')}>
+                    <div className={cx('status-bar')}>
+                        <div className={cx('circle')}>1</div>
+                        <hr className={cx('line')} />
+                        <div className={cx('circle')}>2</div>
+                        <hr className={cx('line')} />
+                        <div className={cx('circle')}>3</div>
+                        <hr className={cx('line')} />
+                        <div className={cx('circle')}>4</div>
+                        <hr className={cx('line')} />
+                        <div className={cx('circle')}>5</div>
+                    </div>
+                </div>
+
+                <div className={cx('status-text')}>CHỜ XÁC NHẬN</div>
+            </div>
+
+            <div className={cx('customer-info')}>
+                <div className={cx('title')}>
+                    <div className={cx('icon-customer-info')}></div>
+                    <span className={cx('text-title')}>THÔNG TIN NGƯỜI NHẬN</span>
+                </div>
+
+                <div className={cx('detail-info')}>
+                    <div className={cx('detail-title')}>
+                        <span className={cx('title-info')}>Tên khách hàng:</span>
+                        <span className={cx('title-info')}>Số điện thoại:</span>
+                        <span className={cx('title-info')}>Địa chỉ:</span>
+                    </div>
+
+                    <div className={cx('detail')}>
+                        <span className={cx('info')}>Hoàng Phúc</span>
+                        <span className={cx('info')}>0123456789</span>
+                        <span className={cx('info')}>Xã Tân Lập, Dĩ An, Bình Dương</span>
+                    </div>
+                </div>
+            </div>
+
+            {
+                isOpen
+                    ?
+                    <div className={cx('product-list')}>
+                        <div className={cx('title-wrapper-open')}>
+                            <div className={cx('title')}>
+                                <div className={cx('icon-product-list')}></div>
+                                <span className={cx('text-title')}>DANH SÁCH SẢN PHẨM</span>
+                            </div>
+                            <div className={cx('icon-sortdown')} onClick={closeProductList}></div>
+                        </div>
+
+                        <div className={cx('detail-product')}>
+                            <img className={cx('product-image')} src={image} alt='ProductImage' />
+                            <div className={cx('product-info')}>
+                                <span>Áo Real Madrid</span>
+                                <span>Áo Real Madrid</span>
+                                <span>Áo Real Madrid</span>
+                                <span>Áo Real Madrid</span>
+                            </div>
+                        </div>
+                    </div>
+                    :
+                    <div className={cx('product-list')}>
+                        <div className={cx('title-wrapper')}>
+                            <div className={cx('title')}>
+                                <div className={cx('icon-product-list')}></div>
+                                <span className={cx('text-title')}>DANH SÁCH SẢN PHẨM</span>
+                            </div>
+                            <div className={cx('icon-sortdown')} onClick={openProductList}></div>
+                        </div>
+                    </div>
+
+
+            }
+
+            <div className={cx('delivery-payment')}>
+                <div className={cx('title')}>
+                    <div className={cx('icon-delivery-payment')}></div>
+                    <span className={cx('text-title')}>PHƯƠNG THỨC VẬN CHUYỂN & THANH TOÁN</span>
+                </div>
+            </div>
+
+        </div>
     );
 }
 
